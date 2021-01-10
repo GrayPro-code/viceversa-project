4ac5f3e6934e970fa6eb8db158e762adb90736f5
gray.pro <gray.aloner@icloud.com>
2021-01-10 12:50:40 +0100

    last commit

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	rever = user_text[: :-1]
+	s = user_text.split()
+	l = len(s)
+	
+	return render(request, 'reverse.html', {'text':user_text,'text1':l, 'text2':rever})
